1439838cf750d5b82fc22778578386828338a62d
wuxinyiwu <2743508034@qq.com>
2018-09-14 11:11:11 +0800

    指明这是分支

--- gitTest/src/gittest/Git.java
+++ gitTest/src/gittest/Git.java
@@ -4,7 +4,7 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git branch");
+		System.out.println("hello git branch，this is branch");
 	}
 
 }
