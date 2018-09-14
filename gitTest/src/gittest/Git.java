1d89687853847ac4848870a7c7ab2b47480416a1
wuxinyiwu <2743508034@qq.com>
2018-09-14 11:25:41 +0800

    没什么变化

--- gitTest/src/gittest/Git.java
+++ gitTest/src/gittest/Git.java
@@ -4,7 +4,9 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git branch，this is master");
+		
+		System.out.println("hello git，this is merge version");
+
 	}
 
 }
