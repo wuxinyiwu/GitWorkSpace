07adcede238958ebe754b3961474df8ce4df1072
wuxinyiwu <2743508034@qq.com>
2018-12-10 08:56:35 +0800

    兼容调整

--- servlet Maven Webapp/src/main/java/listen/bean/BindingListenerBean.java
+++ servlet Maven Webapp/src/main/java/listen/bean/BindingListenerBean.java
@@ -21,7 +21,6 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class BindingListenerBean implements HttpSessionBindingListener{
     private String name;
     public BindingListenerBean(String name){
--- servlet Maven Webapp/src/main/java/listen/bean/SerializListener.java
+++ servlet Maven Webapp/src/main/java/listen/bean/SerializListener.java
@@ -19,7 +19,6 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @WebListener
 
 public class SerializListener implements HttpSessionActivationListener,  Serializable{
